f1ff3e45c445fb474319009d5045903ba091e910
Jack Works <jackworks@protonmail.com>
2021-08-24 14:36:15 +0800

    chore: improve code

--- packages/maskbook/src/plugins/Collectible/SNSAdaptor/Collectible.tsx
+++ packages/maskbook/src/plugins/Collectible/SNSAdaptor/Collectible.tsx
@@ -184,8 +184,8 @@ export function Collectible(props: CollectibleProps) {
         <Tab className={classes.tab} key="history" label={t('plugin_collectible_history')} />,
     ]
 
-    const isEndTimeVisible =
-        asset.value?.end_time && isValidDate(asset.value.end_time) && isAfter(asset.value.end_time, Date.now())
+    const endDate = asset.value?.end_time
+    const isEndTimeVisible = endDate && isValidDate(endDate) && isAfter(endDate, Date.now())
     return (
         <>
             <CollectibleCard classes={classes}>
@@ -307,7 +307,7 @@ export function Collectible(props: CollectibleProps) {
                 <Box sx={{ marginTop: 1 }}>
                     <Typography className={classes.countdown}>
                         {t('plugin_collectible_sale_end', {
-                            time: formatDateTime(asset.value?.end_time!, 'yyyy-MM-dd HH:mm:ss'),
+                            time: formatDateTime(endDate, 'yyyy-MM-dd HH:mm:ss'),
                         })}
                     </Typography>
                 </Box>
